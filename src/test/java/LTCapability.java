743392468d113ca7ecee5fb421f1bfeb8dedc856
Rahul Mishra <148062754+rahulmishra-lt@users.noreply.github.com>
2026-01-07 18:16:59 +0530

    Merge pull request #11 from rahulmishra-lt/main
    updated: deps to junit 5 and latest playwright versions

--- src/test/java/LTCapability.java
+++ src/test/java/LTCapability.java
@@ -1,18 +1,17 @@
 import com.google.gson.JsonObject;
-import com.tngtech.java.junit.dataprovider.DataProvider;
+
+import java.util.stream.Stream;
 
 public class LTCapability {
-  @DataProvider
-  public static Object[] getDefaultTestCapability() {
+  public static Stream<JsonObject> getDefaultTestCapability() {
     JsonObject capabilities1 = new JsonObject();
     JsonObject ltOptions1 = new JsonObject();
 
-    String user = System.getenv("LT_USERNAME") == null ? "LT_USERNAME" :
-            System.getenv("LT_USERNAME");
-    String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "LT_ACCESS_KEY" :
-            System.getenv("LT_ACCESS_KEY");
+    String user = System.getenv("LT_USERNAME") == null ? "LT_USERNAME" : System.getenv("LT_USERNAME");
+    String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "LT_ACCESS_KEY" : System.getenv("LT_ACCESS_KEY");
 
-    capabilities1.addProperty("browserName", "Chrome"); // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+    capabilities1.addProperty("browserName", "Chrome"); // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`,
+                                                        // `pw-firefox` and `pw-webkit`
     capabilities1.addProperty("browserVersion", "latest");
     ltOptions1.addProperty("platform", "Windows 10");
     ltOptions1.addProperty("name", "Playwright HyperExecute Test");
@@ -24,8 +23,6 @@ public class LTCapability {
     ltOptions1.addProperty("visual", true);
     capabilities1.add("LT:Options", ltOptions1);
 
-    return new Object[]{
-      capabilities1
-    };
+    return Stream.of(capabilities1);
   }
 }
